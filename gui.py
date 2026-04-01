adde7897f55da54cc80ab6cf687d8886f824df7e
Tarun Tyagi <tarun.tyagi1978@gmail.com>
2026-04-01 12:20:06 -0700

    feat:add-edit-functionality

--- gui.py
+++ gui.py
@@ -4,18 +4,20 @@ from modules import utils
 #import the module
 import FreeSimpleGUI as sg
 
-#set the layout
-layout =[
-    [
-        sg.Text("Type in a To-Do")],
-        [
-            sg.Input(tooltip="Enter Todo",key="todo"),
-            sg.Button('Add')
-        ],
-]
-
 file_path = Path('todo.txt')
 file_path.touch(exist_ok = True)
+
+label =  sg.Text("Type in a To-Do")
+input_box = sg.Input(tooltip="Enter the Task", key="todo")
+add_button = sg.Button("Add")
+list_box = sg.Listbox(values = utils.read_files(file_path), key="todos", enable_events=True, size=[45,10])
+edit_button= sg.Button("Edit")
+
+
+#set the layout
+layout =[[label ],[input_box, add_button],[list_box,edit_button]]
+
+
 
 window = sg.Window("MY TODO APP ",
                    layout,
@@ -31,6 +33,22 @@ while True:
                 todos = utils.read_files(file_path)
                 todos.append(values['todo']+ "\n")
                 utils.write_to_file(todos,file_path)
+                window['todo'].update(value="")
+                window['todos'].update(values=todos)
+
+
+            case "Edit":
+                todos_from_file = utils.read_files(file_path)
+                todo_to_edit= values['todos'][0]
+                index = todos_from_file.index(todo_to_edit)
+                new_todo= values['todo'] or todo_to_edit
+                todos_from_file[index] = new_todo + "\n"
+                utils.write_to_file(todos_from_file, file_path)
+                window['todos'].update(values =  todos_from_file)
+
+            case "todos":
+                new_value = values["todos"][0]
+                window['todo'].update(value = new_value.strip())
 
             case sg.WINDOW_CLOSED:
                 break
